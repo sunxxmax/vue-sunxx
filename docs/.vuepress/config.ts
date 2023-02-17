24f5dea6afc8607b2a40c80666494a369375b60c
sunxxmax <sunxxmax@gmail.com>
2023-02-17 10:58:06 +0800

    添加编辑操作

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,6 +7,13 @@ export default defineUserConfig({
     title: '',
     description: '',
     theme: defaultTheme({
+        docsRepo: 'https://github.com/sunxxmax/vue-sunxx',
+        docsBranch: 'feature',
+        docsDir: 'docs',
+        editLinkPattern: ':repo/edit/:branch/:path',
+        lastUpdatedText: '更新时间',
+        editLinkText: '编辑此页',
+        contributors: false,
         navbar: [
             // NavbarItem
             {
